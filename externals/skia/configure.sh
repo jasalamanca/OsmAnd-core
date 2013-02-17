1648a616cefcf44d5d064661ae676c8d5a94a419
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-17 17:59:45 +0200

    Update external dependency scripts

--- externals/skia/configure.sh
+++ externals/skia/configure.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if ls -1 $SRCLOC/upstream.patched >/dev/null 2>&1
-then
-   exit
+NAME=$(basename $(dirname "${BASH_SOURCE[0]}") )
+
+# Check if already configured
+if [ -d "$SRCLOC/upstream.patched" ]; then
+	echo "Skipping external '$NAME': already configured"
+	exit
 fi
 
-# Delete old one if such exists
-if ls -1 $SRCLOC/upstream.* >/dev/null 2>&1
-then
-	echo "Deleting old upstream..."
-	rm -rf $SRCLOC/upstream.*
-fi
-
-# Download
-echo "Downloading new upstream..."
-VERSION="chromium-26.0.1408.1"
-mkdir -p "$SRCLOC/upstream.original"
-(cd "$SRCLOC/upstream.original" && \
+# Extract upstream if needed
+if [ ! -d "$SRCLOC/upstream.original" ]; then
+	echo "Downloading '$NAME' upstream..."
+	VERSION="chromium-26.0.1408.1"
+	mkdir -p "$SRCLOC/upstream.original"
+	(cd "$SRCLOC/upstream.original" && \
 	git init && \
 	git remote add origin -t $VERSION https://github.com/osmandapp/OsmAnd-external-skia.git && \
 	git fetch --depth=1 && \
 	git checkout $VERSION)
+fi
 
 # Patch
-cp -rf $SRCLOC/upstream.original $SRCLOC/upstream.patched
-if [ -d $SRCLOC/patches ]; then
-	echo "Patching..."
+cp -rf "$SRCLOC/upstream.original" "$SRCLOC/upstream.patched"
+if [ -d "$SRCLOC/patches" ]; then
+	echo "Patching '$NAME'..."
 	PATCHES=`ls -1 $SRCLOC/patches/*.patch | sort`
 	for PATCH in $PATCHES
 	do
 		read  -rd '' PATCH <<< "$PATCH"
 		echo "Applying "`basename $PATCH`
-		patch --strip=1 --directory=$SRCLOC/upstream.patched/ --input=$PATCH
+		patch --strip=1 --directory="$SRCLOC/upstream.patched/" --input="$PATCH"
 	done
 fi

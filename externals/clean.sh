957854afa34fbd57638b859ef4009100707ecfa1
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-22 13:24:54 +0300

    Update clean.sh

--- externals/clean.sh
+++ externals/clean.sh
@@ -6,6 +6,6 @@ for external in $SRCLOC/* ; do
 	if ls -1 $external/upstream.* >/dev/null 2>&1
 	then
 		echo "Cleaning '"$(basename "$external")"'..."
-		rm -rf $external/upstream.*
+		rm -rf "$external"/upstream.*
 	fi
 done

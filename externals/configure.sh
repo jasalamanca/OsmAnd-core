b13bffb4495386f9fb30c57bf0cd5b718e460493
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-22 13:25:13 +0300

    Update configure.sh

--- externals/configure.sh
+++ externals/configure.sh
@@ -5,7 +5,7 @@ SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for external in $SRCLOC/* ; do
 	if [ -d "$external" ]; then
 		if [ -e "$external/configure.sh" ]; then
-			$external/configure.sh
+			"$external/configure.sh"
 		fi
 	fi
 done
